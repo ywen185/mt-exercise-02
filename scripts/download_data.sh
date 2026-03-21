667a268fa729e1ef71225716519c77390f97316c
ywen185 <wen.yang@uzh.ch>
2026-03-21 17:39:07 +0100

    Update download_data.sh

--- scripts/download_data.sh
+++ scripts/download_data.sh
@@ -24,7 +24,7 @@ mkdir -p $data/grimm
 
 mkdir -p $data/grimm/raw
 
-wget https://www.gutenberg.org/files/52521/52521-0.txt
+wget https://www.gutenberg.org/cache/epub/78260/pg78260.txt
 mv 52521-0.txt $data/grimm/raw/tales.txt
 
 # preprocess slightly
